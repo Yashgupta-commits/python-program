a097014a09ae1206ed1fbb8bf172ae0c4c7a6a43
Yashgupta-commits <yashgupta0197@gmail.com>
2025-06-09 12:38:35 +0530

    Update task1.py

--- task1.py
+++ task1.py
@@ -7,7 +7,7 @@ subtraction = num1 - num2
 multiplication = num1 * num2
 division = num1 / num2
 
-
+print("Addition:", addition)
 print("Subtraction:", subtraction)
 print("Multiplication:", multiplication)
 print("Division:", division)
